ac97047817995853c8eceab5526661363b04583a
yojana2003 <yojanachaudhari03@gmail.com>
2026-06-01 12:35:37 +0530

    Restructure project into backend and frontend folders

--- config/token.js
+++
@@ -1,8 +0,0 @@
-const jwt=require("jsonwebtoken")
-
-const generateToken= (id)=>{
-    let token =jwt.sign({id}, process.env.JWT_SCREAT, {expiresIn:"7d"})
-    return token
-}
-
-module.exports = generateToken
